611493529ac93f3248ee40c4bfeb1bbf3ee7454a
Niall Byrne <nbyrne@OLM-NBYRNE.local>
2014-12-10 18:53:40 -0500

    Combined the deployment and content repos.

--- deploy/provisioning/provision.sh
+++ deploy/provisioning/provision.sh
@@ -14,14 +14,9 @@ sudo apt-get -y install ansible
 
 # Pull down the configuration Repo
 cd $GITROOT
-git clone https://github.com/niall-byrne/pancancer.info.config.git
-
-# Extend ansible to handle cpan installations
-mkdir $GITROOT/pancancer.info.config/provisioning/library
-cd $GITROOT/pancancer.info.config/provisioning/library
-wget https://raw.githubusercontent.com/ansible/ansible-modules-extras/317654dba5cae905b5d6eed78f5c6c6984cc2f02/packaging/language/cpanm.py
+git clone https://github.com/ICGC-TCGA-PanCancer/pancancer.info.content.git
 
 # Execute the playbook
-cd $GITROOT/pancancer.info.config/provisioning
+cd $GITROOT/pancancer.info.content/deploy/provisioning
 ansible-playbook -i inventory site.yml
 
