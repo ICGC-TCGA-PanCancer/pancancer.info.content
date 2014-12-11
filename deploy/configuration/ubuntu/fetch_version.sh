05d39cb18695d3924d183b8d10538771f6e5d1ed
Niall Byrne <nbyrne@OLM-NBYRNE.local>
2014-12-10 22:24:34 -0500

    .

--- deploy/configuration/ubuntu/fetch_version.sh
+++ deploy/configuration/ubuntu/fetch_version.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# A silly method of determining the Ubuntu Version accross all releases
+
+# A silly method of determining the Ubuntu Version across all releases
 egrep -o DISTRIB_RELEASE=[0-9]+.[0-9]+ /etc/lsb-release | egrep -o [0-9]+.[0-9]+
 
